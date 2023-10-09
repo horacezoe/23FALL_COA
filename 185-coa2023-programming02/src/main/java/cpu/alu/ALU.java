0cc09e5ce10b92566dd583fe42f489c7b7e835a0
Administrator <admin@seecoder.cn>
2023-10-09 11:08:10 +0800

    初始化

---
+++ 185-coa2023-programming02/src/main/java/cpu/alu/ALU.java
@@ -0,0 +1,37 @@
+package cpu.alu;
+
+import util.DataType;
+
+/**
+ * Arithmetic Logic Unit
+ * ALU封装类
+ */
+public class ALU {
+
+    /**
+     * 返回两个二进制整数的和
+     * dest + src
+     *
+     * @param src  32-bits
+     * @param dest 32-bits
+     * @return 32-bits
+     */
+    public DataType add(DataType src, DataType dest) {
+        // TODO
+        return null;
+    }
+
+    /**
+     * 返回两个二进制整数的差
+     * dest - src
+     *
+     * @param src  32-bits
+     * @param dest 32-bits
+     * @return 32-bits
+     */
+    public DataType sub(DataType src, DataType dest) {
+        // TODO
+        return null;
+    }
+
+}
