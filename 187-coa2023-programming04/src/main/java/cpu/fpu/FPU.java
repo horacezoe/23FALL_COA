3edc2062bbdb75b40a728d1dc35630db8c291ac7
Administrator <admin@seecoder.cn>
2023-10-22 23:41:55 +0800

    初始化

---
+++ 187-coa2023-programming04/src/main/java/cpu/fpu/FPU.java
@@ -0,0 +1,183 @@
+package cpu.fpu;
+
+import util.DataType;
+import util.IEEE754Float;
+
+/**
+ * floating point unit
+ * 执行浮点运算的抽象单元
+ * 浮点数精度：使用3位保护位进行计算
+ */
+public class FPU {
+
+    private final String[][] addCorner = new String[][]{
+            {IEEE754Float.P_ZERO, IEEE754Float.P_ZERO, IEEE754Float.P_ZERO},
+            {IEEE754Float.N_ZERO, IEEE754Float.P_ZERO, IEEE754Float.P_ZERO},
+            {IEEE754Float.P_ZERO, IEEE754Float.N_ZERO, IEEE754Float.P_ZERO},
+            {IEEE754Float.N_ZERO, IEEE754Float.N_ZERO, IEEE754Float.N_ZERO},
+            {IEEE754Float.P_INF, IEEE754Float.N_INF, IEEE754Float.NaN},
+            {IEEE754Float.N_INF, IEEE754Float.P_INF, IEEE754Float.NaN}
+    };
+
+    private final String[][] subCorner = new String[][]{
+            {IEEE754Float.P_ZERO, IEEE754Float.P_ZERO, IEEE754Float.P_ZERO},
+            {IEEE754Float.N_ZERO, IEEE754Float.P_ZERO, IEEE754Float.N_ZERO},
+            {IEEE754Float.P_ZERO, IEEE754Float.N_ZERO, IEEE754Float.P_ZERO},
+            {IEEE754Float.N_ZERO, IEEE754Float.N_ZERO, IEEE754Float.P_ZERO},
+            {IEEE754Float.P_INF, IEEE754Float.P_INF, IEEE754Float.NaN},
+            {IEEE754Float.N_INF, IEEE754Float.N_INF, IEEE754Float.NaN}
+    };
+
+    private final String[][] mulCorner = new String[][]{
+            {IEEE754Float.P_ZERO, IEEE754Float.N_ZERO, IEEE754Float.N_ZERO},
+            {IEEE754Float.N_ZERO, IEEE754Float.P_ZERO, IEEE754Float.N_ZERO},
+            {IEEE754Float.P_ZERO, IEEE754Float.P_ZERO, IEEE754Float.P_ZERO},
+            {IEEE754Float.N_ZERO, IEEE754Float.N_ZERO, IEEE754Float.P_ZERO},
+            {IEEE754Float.P_ZERO, IEEE754Float.P_INF, IEEE754Float.NaN},
+            {IEEE754Float.P_ZERO, IEEE754Float.N_INF, IEEE754Float.NaN},
+            {IEEE754Float.N_ZERO, IEEE754Float.P_INF, IEEE754Float.NaN},
+            {IEEE754Float.N_ZERO, IEEE754Float.N_INF, IEEE754Float.NaN},
+            {IEEE754Float.P_INF, IEEE754Float.P_ZERO, IEEE754Float.NaN},
+            {IEEE754Float.P_INF, IEEE754Float.N_ZERO, IEEE754Float.NaN},
+            {IEEE754Float.N_INF, IEEE754Float.P_ZERO, IEEE754Float.NaN},
+            {IEEE754Float.N_INF, IEEE754Float.N_ZERO, IEEE754Float.NaN}
+    };
+
+    private final String[][] divCorner = new String[][]{
+            {IEEE754Float.P_ZERO, IEEE754Float.P_ZERO, IEEE754Float.NaN},
+            {IEEE754Float.N_ZERO, IEEE754Float.N_ZERO, IEEE754Float.NaN},
+            {IEEE754Float.P_ZERO, IEEE754Float.N_ZERO, IEEE754Float.NaN},
+            {IEEE754Float.N_ZERO, IEEE754Float.P_ZERO, IEEE754Float.NaN},
+            {IEEE754Float.P_INF, IEEE754Float.P_INF, IEEE754Float.NaN},
+            {IEEE754Float.N_INF, IEEE754Float.N_INF, IEEE754Float.NaN},
+            {IEEE754Float.P_INF, IEEE754Float.N_INF, IEEE754Float.NaN},
+            {IEEE754Float.N_INF, IEEE754Float.P_INF, IEEE754Float.NaN},
+    };
+
+    /**
+     * compute the float add of (dest + src)
+     */
+    public DataType add(DataType src, DataType dest) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * compute the float add of (dest - src)
+     */
+    public DataType sub(DataType src, DataType dest) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * compute the float mul of (dest * src)
+     */
+    public DataType mul(DataType src,DataType dest){
+        //TODO
+        return null;
+    }
+
+    /**
+     * compute the float mul of (dest / src)
+     */
+    public DataType div(DataType src,DataType dest){
+        //TODO
+        return null;
+    }
+
+    /**
+     * check corner cases of mul and div
+     *
+     * @param cornerMatrix corner cases pre-stored
+     * @param oprA first operand (String)
+     * @param oprB second operand (String)
+     * @return the result of the corner case (String)
+     */
+    private String cornerCheck(String[][] cornerMatrix, String oprA, String oprB) {
+        for (String[] matrix : cornerMatrix) {
+            if (oprA.equals(matrix[0]) && oprB.equals(matrix[1])) {
+                return matrix[2];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * right shift a num without considering its sign using its string format
+     *
+     * @param operand to be moved
+     * @param n       moving nums of bits
+     * @return after moving
+     */
+    private String rightShift(String operand, int n) {
+        StringBuilder result = new StringBuilder(operand);  //保证位数不变
+        boolean sticky = false;
+        for (int i = 0; i < n; i++) {
+            sticky = sticky || result.toString().endsWith("1");
+            result.insert(0, "0");
+            result.deleteCharAt(result.length() - 1);
+        }
+        if (sticky) {
+            result.replace(operand.length() - 1, operand.length(), "1");
+        }
+        return result.substring(0, operand.length());
+    }
+
+    /**
+     * 对GRS保护位进行舍入
+     *
+     * @param sign    符号位
+     * @param exp     阶码
+     * @param sig_grs 带隐藏位和保护位的尾数
+     * @return 舍入后的结果
+     */
+    private String round(char sign, String exp, String sig_grs) {
+        int grs = Integer.parseInt(sig_grs.substring(24, 27), 2);
+        if ((sig_grs.substring(27).contains("1")) && (grs % 2 == 0)) {
+            grs++;
+        }
+        String sig = sig_grs.substring(0, 24); // 隐藏位+23位
+        if (grs > 4) {
+            sig = oneAdder(sig);
+        } else if (grs == 4 && sig.endsWith("1")) {
+            sig = oneAdder(sig);
+        }
+
+        if (Integer.parseInt(sig.substring(0, sig.length() - 23), 2) > 1) {
+            sig = rightShift(sig, 1);
+            exp = oneAdder(exp).substring(1);
+        }
+        if (exp.equals("11111111")) {
+            return sign == '0' ? IEEE754Float.P_INF : IEEE754Float.N_INF;
+        }
+
+        return sign + exp + sig.substring(sig.length() - 23);
+    }
+
+    /**
+     * add one to the operand
+     *
+     * @param operand the operand
+     * @return result after adding, the first position means overflow (not equal to the carry to the next)
+     *         and the remains means the result
+     */
+    private String oneAdder(String operand) {
+        int len = operand.length();
+        StringBuilder temp = new StringBuilder(operand);
+        temp.reverse();
+        int[] num = new int[len];
+        for (int i = 0; i < len; i++) num[i] = temp.charAt(i) - '0';  //先转化为反转后对应的int数组
+        int bit = 0x0;
+        int carry = 0x1;
+        char[] res = new char[len];
+        for (int i = 0; i < len; i++) {
+            bit = num[i] ^ carry;
+            carry = num[i] & carry;
+            res[i] = (char) ('0' + bit);  //显示转化为char
+        }
+        String result = new StringBuffer(new String(res)).reverse().toString();
+        return "" + (result.charAt(0) == operand.charAt(0) ? '0' : '1') + result;  //注意有进位不等于溢出，溢出要另外判断
+    }
+
+}
