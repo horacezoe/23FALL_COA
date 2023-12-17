d5c092e32a0cb9eef4b0325b638d5f2fd082ada1
Administrator <admin@seecoder.cn>
2023-12-17 21:06:08 +0800

    初始化

---
+++ 191-coa2023-programming09/src/main/java/cpu/nbcdu/NBCDU.java
@@ -0,0 +1,28 @@
+package cpu.nbcdu;
+
+import util.DataType;
+
+public class NBCDU {
+
+    /**
+     * @param src  A 32-bits NBCD String
+     * @param dest A 32-bits NBCD String
+     * @return dest + src
+     */
+    DataType add(DataType src, DataType dest) {
+        // TODO
+        return null;
+    }
+
+    /***
+     *
+     * @param src A 32-bits NBCD String
+     * @param dest A 32-bits NBCD String
+     * @return dest - src
+     */
+    DataType sub(DataType src, DataType dest) {
+        // TODO
+        return null;
+    }
+
+}
